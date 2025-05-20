4b0939ed2e423675be8025adab874d8d5967aaa6
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2025-05-19 20:38:32 -0500

    fdgshdfgh

--- vcpkg_config/triplets/x86-linux.cmake
+++ vcpkg_config/triplets/x86-linux.cmake
@@ -5,4 +5,4 @@ set(VCPKG_BUILD_TYPE release)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../cmake/LinuxToolchain.cmake")
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/cmake/LinuxToolchain.cmake")
