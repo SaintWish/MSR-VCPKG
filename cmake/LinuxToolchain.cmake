84950771683933d58b2d04f37b5e819916a51178
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2025-05-19 20:30:59 -0500

    fxgnh

--- cmake/LinuxToolchain.cmake
+++ cmake/LinuxToolchain.cmake
@@ -1,7 +1,4 @@
 # VCPKG
 set(VCPKG_TARGET_TRIPLET "x86-linux_custom" CACHE INTERNAL "Triplet to use for Linux development. Do not modify.")
-set(CMAKE_C_COMPILER testd)
-set(CMAKE_CXX_COMPILER testdg++)
-set(CMAKE_CXX_COMPILER_FORCED true)
 
 include(${CMAKE_CURRENT_LIST_DIR}/../vcpkg/scripts/buildsystems/vcpkg.cmake)
--- vcpkg_config/triplets/x86-linux_custom.cmake
+++ vcpkg_config/triplets/x86-linux_custom.cmake
@@ -3,6 +3,10 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_BUILD_TYPE release)
 
+set(CMAKE_C_COMPILER i686-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER i686-linux-gnu-g++)
+set(CMAKE_CXX_COMPILER_FORCED true)
+
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../cmake/LinuxToolchain.cmake")
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
