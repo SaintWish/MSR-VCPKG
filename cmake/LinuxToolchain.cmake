740e1a933e51da2516696ea224fdf7f9ff08a427
Saint Wish <6036821+SaintWish@users.noreply.github.com>
2025-05-19 20:35:51 -0500

    zdgsbf

--- cmake/LinuxToolchain.cmake
+++ cmake/LinuxToolchain.cmake
@@ -1,4 +1,8 @@
 # VCPKG
-set(VCPKG_TARGET_TRIPLET "x86-linux_custom" CACHE INTERNAL "Triplet to use for Linux development. Do not modify.")
+set(VCPKG_TARGET_TRIPLET "x86-linux" CACHE INTERNAL "Triplet to use for Linux development. Do not modify.")
+
+set(CMAKE_C_COMPILER i686-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER i686-linux-gnu-g++)
+set(CMAKE_CXX_COMPILER_FORCED true)
 
 include(${CMAKE_CURRENT_LIST_DIR}/../vcpkg/scripts/buildsystems/vcpkg.cmake)
